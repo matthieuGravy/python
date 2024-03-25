a7415475dba35ee65e8b137484083483695f23df
matthieuGravy <matthieugravy@gmail.com>
2024-03-25 15:50:25 +0100

    panel admin

--- digit/blog/models.py
+++ digit/blog/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
 
     def __str__(self):
         return self.title
--- digit/blog/views.py
+++ digit/blog/views.py
@@ -2,14 +2,19 @@
 """""
 on peut aussi ajouter en 3e param le contexte
 """""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from .mocks import Post
 from .models import Post
+from django.http import Http404
 
 def index(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', {'posts': posts})
 
 def show(request, id):
-    post = Post.objects.get(id=id)
+    post = get_object_or_404(Post, id=id)
+    #try:
+    #      post = Post.objects.get(id=id)
+    #except :
+    #    raise Http404("Post does not exist")
     return render(request, 'blog/show.html', {'post': post})
